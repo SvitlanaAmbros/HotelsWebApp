372f22575c1c3ad4c1c765604c9e3f870c60d757
SvitlanaAmbros <svitlana.ambros@gmail.com>
2019-03-22 17:44:16 +0200

    create main page detail component, main page module

---
+++ src/app/main-page/main-page.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from "@angular/core";
+import { SharedModule } from "../shared/shared.module";
+
+
+@NgModule({
+    imports: [
+        SharedModule
+    ], 
+    providers: [
+
+    ],
+    exports: [
+
+    ]
+})
+
+export class MainPageModule { }
