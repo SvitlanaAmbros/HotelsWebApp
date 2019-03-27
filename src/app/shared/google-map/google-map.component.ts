ca0923eca3152bb86a1d41b3f37d8a80de60fd13
SvitlanaAmbros <svitlana.ambros@gmail.com>
2019-03-27 18:46:36 +0200

    create google map component structure

--- src/app/shared/google-map/google-map.component.ts
+++ src/app/shared/google-map/google-map.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+declare const google: any;
 
 @Component({
-  selector: 'app-google-map',
+  selector: 'google-map',
   templateUrl: './google-map.component.html',
   styleUrls: ['./google-map.component.scss']
 })
 export class GoogleMapComponent implements OnInit {
 
+  @Input() latitude: number = 32.7767;
+  @Input() longitude: number = -96.7970;
+  @Input() cityName: string = "City";
+
   constructor() { }
 
   ngOnInit() {
-  }
+    const city = {lat: this.latitude, lng: this.longitude};
 
+    let map = new google.maps.Map(document.getElementById('map'), {
+        center: city,
+        zoom: 15
+    });
+
+    let marker = new google.maps.Marker({
+        position: city,
+        map: map,
+        title: this.cityName 
+    });
+  }
 }
