7628d5c09ff82f601511c7c0a37f3753e036cc37
SvitlanaAmbros <svitlana.ambros@gmail.com>
2019-04-11 12:59:53 +0300

    add form module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
-import { ChooseUsComponent } from './about-us/choose-us/choose-us.component';
-import { OurServicesComponent } from './about-us/our-services/our-services.component';
-import { AboutUsDetailComponent } from './about-us/about-us-detail/about-us-detail.component';
-import { ContactsDetailComponent } from './contacts/contacts-detail/contacts-detail.component';
-import { TouristsDetailComponent } from './tourists/tourists-detail/tourists-detail.component';
-import { TouristsQuestionsComponent } from './tourists/tourists-questions/tourists-questions.component';
 import { AppRoutingModule } from './app.routing.module';
-import { MainPageDetailComponent } from './main-page/main-page-detail/main-page-detail.component';
 import { AboutUsModule } from './about-us/about-us.module';
-import { CountriesComponent } from './tourists/countries/countries.component';
-import { CountryCardComponent } from './tourists/country-card/country-card.component';
 import { ContactsModule } from './contacts/contacts.module';
 import { FooterComponent } from './footer/footer.component';
 import {Ng2PageScrollModule} from 'ng2-page-scroll';
@@ -31,6 +23,7 @@ import { TouristsModule } from './tourists/tourists.module';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AngularFontAwesomeModule,
     Ng2PageScrollModule,
     ScrollToModule.forRoot(),
