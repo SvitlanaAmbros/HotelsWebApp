65c845f173ad9f1b4b857df59961ba79ab5fd5fd
SvitlanaAmbros <svitlana.ambros@gmail.com>
2019-05-12 16:21:42 +0300

    move google-map component in shared components

--- src/app/shared/components/google-map/google-map.component.ts
+++ src/app/shared/components/google-map/google-map.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { IdGeneratorService } from '../services/id-generator.service';
+import { IdGeneratorService } from '../../services/id-generator.service';
 
 declare const google: any;
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { ImageSliderComponent } from "./components/image-slider/image-slider.component";
 import { ExpandingListItemComponent } from './components/expanding-list-item/expanding-list-item.component';
 import { CommonModule } from "@angular/common";
-import { GoogleMapComponent } from './google-map/google-map.component';
+import { GoogleMapComponent } from './components/google-map/google-map.component';
 import { IdGeneratorService } from "./services/id-generator.service";
 import { BtnPrimaryComponent } from './components/btn-primary/btn-primary.component';
 import { FormPopupComponent } from './components/form-popup/form-popup.component';
